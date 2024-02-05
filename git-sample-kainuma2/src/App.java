833e4d2e4dbf769f632bb2a3d3a6eee4ee8dd4cd
‘yuto’_0503 <y.yuto.0503@gmail.comgit config --global user.name yuto_0503git config --global user.email y.yuto.0503@gmail.com>
2024-02-05 11:48:13 +0900

    firstcommit

--- git-sample-kainuma2/src/App.java
+++ git-sample-kainuma2/src/App.java
@@ -1,5 +1,8 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.println("1:aaa");
+        System.out.println("2:eee");
+        System.out.println("3:ccc");
+
     }
 }
